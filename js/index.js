69ce75229c0a13d07f68b1bb9cb0caa7ea0d2c97
nahuel argüello <nahuelarg93@gmail.com>
2024-10-02 20:10:28 -0300

    Innitial

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// se ingresa el nombre de usuario Nahuelarg93
+
+
+function userValidator() {
+    let name;
+    
+        do{
+            name = prompt("Por favor ingrese su nombre de usuario");
+            if (name == "Nahuelarg93") {
+                alert("Dato ingresado correcto");
+            }
+            else {
+                alert("dato ingresado incorrecto ");
+            }
+            
+        } while (name !== "Nahuelarg93");
+            console.log("usuario correcto");
+        
+    }
+
+// function userValidatorCard() {
+//     let card;
+//     do{
+//         card = prompt("Por favor ingrese los 16 numeros de su tarjeta");
+//         if (card.length == 16 && !isNaN(card)){ 
+//             alert("Tarjeta ingresada correctamente");
+//         // alert("Tarjeta ingresada correctamente:", card);
+//     }else {
+//         alert("Debe ingresar los 16 digitos");
+//     }
+// } while (card.length !== 16);
+// console.log("tarjeta correcta");
+
+// }
+userValidator(); 
+//userValidatorCard();
